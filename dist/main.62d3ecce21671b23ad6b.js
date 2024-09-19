4fc2077cbf70943a244efb823e602f17124f8189
Vasiliy Balov <ballov_v@mail.ru>
2024-09-20 02:07:13 +0300

    make task

--- dist/main.62d3ecce21671b23ad6b.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";const t=document.getElementById("app");["item1","item2","item1"].forEach((e=>{const n=document.createElement("div");n.textContent=e,n.classList.add="item",t.appendChild(n)}))})();
